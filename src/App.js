7ab40d7e75ce4f445172794240bba4bf78ed1d30
Zach Gotsch <zach@flexport.com>
2017-12-23 17:18:31 -0500

    Router harness

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+
+const Landing = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+    <div>
+      <h2>Landing</h2>
+    </div>
+  </div>
+);
+
+const Draft = () => (
+  <div>
+    <h2>Draft</h2>
+  </div>
+);
+
+const StandinNav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Landing</Link>
+        </li>
+        <li>
+          <Link to="/draft">Draft</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <StandinNav />
+          <Route exact path="/" component={Landing} />
+          <Route path="/draft" component={Draft} />
+        </div>
+      </Router>
     );
   }
 }
